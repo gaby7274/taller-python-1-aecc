f03a40c57a3fa6f33cd0db0986134390eeba4eb2
Eduardo Santin <eduardo.santin@upr.edu>
2022-09-13 01:48:55 -0300

    added index and user templates

--- index.py
+++ index.py
@@ -1,14 +1,18 @@
 from flask import Flask, redirect, url_for, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='html-templates')
 
-lista_interes = ['Bears', 'Coffee', 'Python', 'League of Legends', 'Jazz', 'Villano Antillano']
+lista_interes = ['Bears', 'Coffee', 'Python', 'League of Legends', 'Jazz', 'Villana Antillano']
 name = 'Nat'
 
-
+# pass the list to the template
 @app.route('/')
-def index(list, name):
+def index():
     return render_template('index.html')
+
+@app.route('/<name>')
+def user(name):	
+    return render_template('user.html', name=name, lista_interes=lista_interes)
 
 if __name__ == '__main__':
     app.run(debug=True)
